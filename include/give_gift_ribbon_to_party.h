a17ba7a02ee2b9ebda6707bfa6f457527d6e9628
ketchup235 <kasyaptu@gmail.com>
2026-07-27 14:42:22 -0400

    clean

--- include/give_gift_ribbon_to_party.h
+++
@@ -1,6 +0,0 @@
-#ifndef GUARD_GIVE_GIFT_RIBBON_TO_PARTY_H
-#define GUARD_GIVE_GIFT_RIBBON_TO_PARTY_H
-
-void GiveGiftRibbonToParty(u8 index, u8 ribbonId);
-
-#endif // GUARD_GIVE_GIFT_RIBBON_TO_PARTY_H
